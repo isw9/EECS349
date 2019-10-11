9c69f653236a603b2dc9728c9bf888a6ffda6c0b
isaac <isw9@case.edu>
2019-10-10 23:09:49 -0400

    Add files for HW 2

---
+++ PE-Import.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+  if( access( "C:\\PE-1.txt", F_OK ) != -1 ) {
+    // file exists
+    if (containsLine() == 1) {
+      int character;
+      FILE *fp;
+      fp = fopen("C:\\PE-1.txt", "r");
+      if (fp) {
+        while ((character = getc(fp)) != EOF) {
+          putchar(character);
+        }
+        fclose(fp);
+      }
+    }
+    else {
+      FILE *fp;
+      fp = fopen("C:\\PE-1.txt", "a");
+      fprintf(fp, "\nI want to learn PE File Format!\n");
+      fclose(fp);
+    }
+  }
+  else {
+    // file doesn't exist
+    FILE *fp;
+
+    fp = fopen("C:\\PE-1.txt", "w+");
+    fprintf(fp, "I want to learn PE File Format!\n");
+    fclose(fp);
+  }
+  return 0;
+}
+
+int containsLine() {
+  char str[1000];
+  FILE *fp;
+  fp = fopen("C:\\PE-1.txt", "r");
+  while (fgets(str, 1000, fp) != NULL) {
+      if (strcmp(str, "I want to learn PE file format!\n") == 0) {
+        return 1;
+      }
+  }
+  fclose(fp);
+  return 0;
+}
