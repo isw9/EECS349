a7232682e0678f13ca6f61c75262dceb18a4a539
isaac <isw9@case.edu>
2019-10-22 21:43:01 -0400

    typo

--- hw2part3/numberOne.c
+++ hw2part3/numberOne.c
@@ -5,7 +5,7 @@ int main(void){
     int param2 = 5;
     int param3 = 0;
 
-    //plus zero because param1 is odd
+    //plus zero because param1 is positive
     param3 = param1 * param2 - ((param1 + (0)) / 2);
 
     printf("%d", param3);
